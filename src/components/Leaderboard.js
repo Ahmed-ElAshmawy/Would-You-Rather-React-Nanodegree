4fba527a8c061324469e785b6731163749723464
Ahmed Hesham <ahm@prooffice.dk>
2020-03-26 20:06:54 +0200

    Update README

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -11,52 +11,48 @@ import {
 
 const trophyColor = ["yellow", "grey", "orange"];
 
-export class Leaderboard extends Component {
-  render() {
-    const { leaderboardData } = this.props;
-
-    return (
-      <Fragment>
-        {leaderboardData.map((user, idx) => (
-          <Segment.Group key={user.id}>
-            <Label corner="left" icon="trophy" color={trophyColor[idx]} />
-            <Grid divided padded>
-              <Grid.Row>
-                <Grid.Column width={4} verticalAlign="middle">
-                  <Image src={user.avatarURL} />
-                </Grid.Column>
-                <Grid.Column width={8}>
-                  <Header as="h3" textAlign="left">
-                    {user.name}
-                  </Header>
-                  <Grid>
-                    <Grid.Column width={12}>Answered questions</Grid.Column>
-                    <Grid.Column width={4}>{user.answerCount}</Grid.Column>
-                  </Grid>
-                  <Divider />
-                  <Grid>
-                    <Grid.Column width={12}>Created questions</Grid.Column>
-                    <Grid.Column width={4}>{user.questionCount}</Grid.Column>
-                  </Grid>
-                </Grid.Column>
-                <Grid.Column width={4} textAlign="center">
-                  <Segment.Group>
-                    <Header as="h5" block attached="top" content="Score" />
-                    <Segment>
-                      <Label circular color="green" size="big">
-                        {user.questionCount + user.answerCount}
-                      </Label>
-                    </Segment>
-                  </Segment.Group>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment.Group>
-        ))}
-      </Fragment>
-    );
-  }
-}
+export const Leaderboard = ({ leaderboardData }) => {
+  return (
+    <Fragment>
+      {leaderboardData.map((user, idx) => (
+        <Segment.Group key={user.id}>
+          <Label corner="left" icon="trophy" color={trophyColor[idx]} />
+          <Grid divided padded>
+            <Grid.Row>
+              <Grid.Column width={4} verticalAlign="middle">
+                <Image src={user.avatarURL} />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Header as="h3" textAlign="left">
+                  {user.name}
+                </Header>
+                <Grid>
+                  <Grid.Column width={12}>Answered questions</Grid.Column>
+                  <Grid.Column width={4}>{user.answerCount}</Grid.Column>
+                </Grid>
+                <Divider />
+                <Grid>
+                  <Grid.Column width={12}>Created questions</Grid.Column>
+                  <Grid.Column width={4}>{user.questionCount}</Grid.Column>
+                </Grid>
+              </Grid.Column>
+              <Grid.Column width={4} textAlign="center">
+                <Segment.Group>
+                  <Header as="h5" block attached="top" content="Score" />
+                  <Segment>
+                    <Label circular color="green" size="big">
+                      {user.questionCount + user.answerCount}
+                    </Label>
+                  </Segment>
+                </Segment.Group>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment.Group>
+      ))}
+    </Fragment>
+  );
+};
 
 function mapStateToProps({ users }) {
   const leaderboardData = Object.values(users)
